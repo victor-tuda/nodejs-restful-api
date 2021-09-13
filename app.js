21ad63fcdc503c705c670abb2a1eb6f13092bf42
victor-tuda <victor_gtuda@outlook.com>
2021-09-13 11:51:23 -0300

    Adicionadaa variavel morgan

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
