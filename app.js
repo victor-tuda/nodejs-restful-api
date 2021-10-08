0366efe53692d205a0e22ee858f12d834131363e
Victor Tuda <victor_gtuda@outlook.com>
2021-10-08 16:42:27 -0300

    fix: Depreceated bodyParser, urlEncoded

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const bodyParser = require("body-parser");
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Alow-Origin', '*');
@@ -14,6 +13,7 @@ app.use((req, res, next) => {
         res.header('Access-Control-Allow_Methods', 'PUT, POST, GET, PATCH, DELETE')
         return res.status(200).json({});
     }
+    next();
 })
 
 const productRoutes = require('./api/routes/products');
