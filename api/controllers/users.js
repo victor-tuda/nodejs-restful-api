ca0e0294066bfa5936f9bb3d2c1b7e869acc2ea0
Victor Tuda <victor_gtuda@outlook.com>
2021-11-16 15:15:32 -0300

    FIX users import model

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,4 +1,6 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
