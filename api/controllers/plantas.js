bf99f43b9e480b1105625c688b9b31715412fd6a
Victor Tuda <victor_gtuda@outlook.com>
2021-11-16 13:16:43 -0300

    ADD controllers em todas as rotas

--- api/controllers/plantas.js
+++ api/controllers/plantas.js
@@ -5,6 +5,42 @@ const checkAuth = require("../middleware/check-auth");
 const Planta = require("../models/planta");
 const Adubo = require("../models/adubo");
 
+exports.plantas_get_by_id = (req, res, next) => {
+    const id = req.params.PlantaId;
+    Planta.findById(id)
+    .exec()
+    .then(doc => {
+        console.log(doc)
+        res.status(200).json(doc)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({error: err})
+    })
+};
+
+exports.plantas_get_all = (req, res, next) => {
+    Planta.find()
+    .exec()
+    .then(docs => {
+        console.log(docs);
+        if (docs.length >= 0){
+            res.status(200).json(docs);
+        }else {
+            res.status(200).json({
+                message: 'Nenhum registro encontrado'
+            });
+        }
+        
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    })
+};
+
 exports.plantas_cadastro = (req, res, next) => {
     const planta = new Planta({
         _id: new mongoose.Types.ObjectId(),
@@ -29,4 +65,39 @@ exports.plantas_cadastro = (req, res, next) => {
         console.log(err);
         res.status(500).json({error:err});
     })
+};
+
+exports.plantas_patch_by_id = (req, res, next) => {
+    const id = req.params.PlantaId;
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value
+    }
+
+    Planta.updateOne({_id: id},{$set: updateOps})
+    .exec()
+    .then(result => {
+        console.log(result);
+        res.status(200).json({result});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    })
+};
+
+exports.plantas_delete_by_id = (req, res, next) => {
+    const id = req.params.PlantaId;
+    Planta.remove({_id: id})
+    .then(result => {
+        res.status(200).json(result)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    });
 };
--- api/routes/plantas.js
+++ api/routes/plantas.js
@@ -9,77 +9,14 @@ const Planta = require('../models/planta');
 
 const PlantasController = require('../controllers/plantas');
 
-router.get('/', (req, res, next) => {
-    Planta.find()
-    .exec()
-    .then(docs => {
-        console.log(docs);
-        if (docs.length >= 0){
-            res.status(200).json(docs);
-        }else {
-            res.status(200).json({
-                message: 'Nenhum registro encontrado'
-            });
-        }
-        
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        })
-    })
-});
+router.get('/', checkAuth, PlantasController.plantas_get_all);
 
 router.post('/', checkAuth, PlantasController.plantas_cadastro);
 
-router.get('/:PlantaId', (req, res, next) => {
-    const id = req.params.PlantaId;
-    Planta.findById(id)
-    .exec()
-    .then(doc => {
-        console.log(doc)
-        res.status(200).json(doc)
-    })
-    .catch(err => {
-        console.log(err)
-        res.status(500).json({error: err})
-    })
-});
+router.get('/:PlantaId', checkAuth, PlantasController.plantas_get_by_id);
 
-router.patch('/:PlantaId', (req, res, next) => {
-    const id = req.params.PlantaId;
-    const updateOps = {};
-    for (const ops of req.body) {
-        updateOps[ops.propName] = ops.value
-    }
+router.patch('/:PlantaId', checkAuth, PlantasController.plantas_patch_by_id);
 
-    Planta.updateOne({_id: id},{$set: updateOps})
-    .exec()
-    .then(result => {
-        console.log(result);
-        res.status(200).json({result});
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        })
-    })
-});
-
-router.delete('/:PlantaId', (req, res, next) => {
-    const id = req.params.PlantaId;
-    Planta.remove({_id: id})
-    .then(result => {
-        res.status(200).json(result)
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        })
-    });
-});
+router.delete('/:PlantaId', checkAuth, PlantasController.plantas_delete_by_id);
 
 module.exports = router;
