138b84b7b1896f5016aeb899fc153f5c53cf8018
Victor Tuda <victor_gtuda@outlook.com>
2021-11-16 13:42:48 -0300

    ADD controllers para users routes

--- api/routes/users.js
+++ api/routes/users.js
@@ -8,99 +8,12 @@ const jwt = require('jsonwebtoken');
 // Importando o modelo de user
 const User = require('../models/user');
 
-router.post('/cadastro', (req, res, next) =>{
-    User.find({email: req.body.email})
-    .exec()
-    .then(user => {
-        if (user.length >= 1) {
-            return res.status(409).json({
-                message: "E-mail cadastrado já existente"
-            })
-        }  
-        else {
-            bcrypt.hash(req.body.password, 10, (err, hash) => {
-                if (err) {
-                    return res.status(500).json({
-                        error: err
-                    });
-                }
-                else {
-                    const user = new User({
-                        _id: new mongoose.Types.ObjectId(),
-                        email: req.body.email,
-                        password: hash
-                    });
-                    user
-                    .save()
-                    .then(result => {
-                        console.log(result);
-                        res.status(201).json({
-                            message: "Usuário criado com sucesso!"
-                        })
-                    })
-                    .catch(err => {
-                        res.status(500).json({
-                            error: err
-                        })
-                    })
-                }   
-            })
-        }
-    })
-});
+const UsersController = require('../controllers/users');
 
-router.post('/login', (req, res, next) => {
-    User.find({email: req.body.email})
-    .exec()
-    .then(user => {
-        if (user.length < 1){
-            res.status(401).json({
-                message: 'Autenticação falhou'
-            })
-        }
-        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-            if (err){
-                res.status(401).json({
-                    message: 'Autenticação falhou'
-                })
-            }
-            if (result){
-                const token = jwt.sign({
-                    email: user[0].email,
-                    userId: user[0]._id
-                },
-                process.env.JWT_KEY,
-                {
-                    expiresIn: "1h"
-                })
-                return res.status(200).json({
-                    message: 'Autenticação autorizada',
-                    token: token
-                })
-            }
-        })
-    })
-    .catch(err => {
-        res.status(500).json({
-            error: err
-        })
-    })
+router.post('/cadastro', UsersController.users_cadastro);
 
-})
+router.post('/login', UsersController.users_login);
 
-router.delete('/:userId', (req, res, next) => {
-    User.remove({_id: req.params.userId})
-    .exec()
-    .then(result => {
-        res.status(200).json({
-            message: 'Usuário excluído com sucesso!'
-        })
-    })
-    .catch(err => {
-        res.status(500).json({
-            error: err
-        })
-    });
-})
+router.delete('/:userId', UsersController.users_delete_by_id);
 
 module.exports = router;
