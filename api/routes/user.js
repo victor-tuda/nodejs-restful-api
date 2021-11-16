5fe0da4233760a3c5b31596fee6088a80cab94e5
Victor Tuda <victor_gtuda@outlook.com>
2021-11-15 21:42:36 -0300

    Token de acesso configurado

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 // Importando o modelo de user
 const User = require('../models/user');
@@ -64,8 +65,17 @@ router.post('/login', (req, res, next) => {
                 })
             }
             if (result){
+                const token = jwt.sign({
+                    email: user[0].email,
+                    userId: user[0]._id
+                },
+                process.env.JWT_KEY,
+                {
+                    expiresIn: "1h"
+                })
                 return res.status(200).json({
-                    message: 'Autenticação autorizada'
+                    message: 'Autenticação autorizada',
+                    token: token
                 })
             }
         })
